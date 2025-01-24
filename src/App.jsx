d8ea8aac835246017229b4777c0b29dd524dd4ef
taejin7088 <gog70888@gmail.com>
2025-01-24 22:23:22 +0900

    userEffect 잘못된코드수정, 유효성검사코드변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,41 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Table from './components/MedalTable.jsx'
-import Form from './components/Form.jsx'
-import './App.css'
-import { ssrModuleExportsKey } from 'vite/module-runner'
-
-
+import { useState, useEffect, useTransition } from "react";
+import Table from "./components/MedalTable.jsx";
+import Form from "./components/Form.jsx";
+import "./App.css";
 
 function App() {
   //처음 랜더링 될 때 로컬스토리지에 있는 0번째의 밸류(배열)를 가져옴
-  const countryListInLocalStorage = JSON.parse(window.localStorage.getItem(0))
-  const [countryList, setConList] = useState(countryListInLocalStorage)
-
+  const countryListInLocalStorage = JSON.parse(window.localStorage.getItem(0));
+  const [countryList, setConList] = useState(countryListInLocalStorage);
 
   //conutryList를 조작하는 함수들
   const handleCountryList = {
     //conutryList에 해당 국가가 있는지 국가명을 기준으로 판별(boolean값 리턴)
     isCountryInList: (targetCountryName) => {
-      return countryList.some((county) => county.countryName === targetCountryName)
+      return countryList.some(
+        (county) => county.countryName === targetCountryName
+      );
     },
 
     //체크되어있는 정렬옵션 기준으로 입력된 배열 정렬실행(리턴값없음)
     sortByMedal: (conlist) => {
       switch (sortOption) {
-        case 'goldMedal':
+        case "goldMedal":
           conlist.sort((a, b) => {
-            return Number(b.goldMedalCount) - Number(a.goldMedalCount)
-          })
+            return Number(b.goldMedalCount) - Number(a.goldMedalCount);
+          });
           break;
-        case 'totalMedal':
+        case "totalMedal":
           conlist.sort((a, b) => {
-            return (Number(b.goldMedalCount) + Number(b.silverMedalCount) + Number(b.bronzeMedalCount)) - (Number(a.goldMedalCount) + Number(a.silverMedalCount) + Number(a.bronzeMedalCount))
-          })
+            return (
+              Number(b.goldMedalCount) +
+              Number(b.silverMedalCount) +
+              Number(b.bronzeMedalCount) -
+              (Number(a.goldMedalCount) +
+                Number(a.silverMedalCount) +
+                Number(a.bronzeMedalCount))
+            );
+          });
           break;
       }
     },
@@ -40,93 +43,115 @@ function App() {
     //입력된 국가객체를 conuntrylist에 추가
     //이미 있는 국가면 false리턴
     addCountry: (newCountry) => {
-      if (handleCountryList.isCountryInList(newCountry.countryName)) {
-        alert("포함 되어있는 나라")
+      console.log(newCountry);
+      const pattern = /^[가-힣]+$/;
+      if (!pattern.test(newCountry.countryName)) {
+        alert("한글국가명만 입력");
         return false;
       }
-      const tempConList = [...countryList, newCountry]
-      handleCountryList.sortByMedal(tempConList)
-      setConList(newConList)
-      alert("추가완료")
+      if (handleCountryList.isCountryInList(newCountry.countryName)) {
+        alert("포함 되어있는 나라");
+        return false;
+      }
+      const tempConList = [...countryList, newCountry];
+      handleCountryList.sortByMedal(tempConList);
+      setConList(tempConList);
+      localStorage.setItem(0, JSON.stringify(tempConList));
+      alert("추가완료");
       return true;
     },
 
     //입력된 나라이름을 기준으로 conuntrylist에서 삭제
     removeCountry: (targetCountryName) => {
-      setConList(countryList.filter((country) => {
+      const tempCountry = countryList.filter((country) => {
         return country.countryName !== targetCountryName;
-      }))
+      });
+      setConList(tempCountry);
+      localStorage.setItem(0, JSON.stringify(tempCountry));
     },
 
     //입력된 국가객체와 똑같은 이름의 국가가 있으면 해당 국가에 해당하는 객체 변경
     //없는 국가면 false리턴
     updateCountry: (targetCountry) => {
       if (!handleCountryList.isCountryInList(targetCountry.countryName)) {
-        alert("포함 되어있지 않은 나라")
+        alert("포함 되어있지 않은 나라");
         return false;
       }
-      const tempConList = countryList.map(country => {
-        if (country.countryName !== targetCountry.countryName) { return country }
+      const tempConList = countryList.map((country) => {
+        if (country.countryName !== targetCountry.countryName) {
+          return country;
+        }
         return {
           countryName: targetCountry.countryName,
           goldMedalCount: targetCountry.goldMedalCount,
           silverMedalCount: targetCountry.silverMedalCount,
           bronzeMedalCount: targetCountry.bronzeMedalCount,
-        }
-      })
-      handleCountryList.sortByMedal(tempConList)
-      setConList(tempConList)
-      alert("변경완료")
+        };
+      });
+      handleCountryList.sortByMedal(tempConList);
+      setConList(tempConList);
+      localStorage.setItem(0, JSON.stringify(tempConList));
+      alert("변경완료");
       return true;
-    }
-  }
+    },
+  };
 
   //정렬옵션을 바꾸면 해당 옵션에 맞게 정렬실행
-  let sortOption = "goldMedal"
+  let sortOption = "goldMedal";
   const handleSortOption = (option) => {
-    sortOption = option
-    const tempConList = [...countryList]
-    handleCountryList.sortByMedal(tempConList)
-    setConList(tempConList)
-  }
+    sortOption = option;
+    const tempConList = [...countryList];
+    handleCountryList.sortByMedal(tempConList);
+    setConList(tempConList);
+  };
 
-  //countryList의 상태가 변경될 때 실행,(처음 렌더링될때도실행(안중요)) 
+  //countryList의 상태가 변경될 때 실행,(처음 렌더링될때도실행(안중요))
   //로컬스토리지에 문자열로 파싱해서 저장
-  useEffect(() => {
-    window.localStorage.setItem(0, JSON.stringify(countryList));
-  }, [countryList]);
+  // useEffect(() => {
+  //   window.localStorage.setItem(0, JSON.stringify(countryList));
+  // }, [countryList]);
+  //쓰면안됨
 
   return (
     <>
       <main>
         <h1>2024 파리 올림픽</h1>
         {/* {입력폼섹션} */}
-
-        <div className='sort-options'>
-          <div className='option'>
+        <div className="sort-options">
+          <div className="option">
             <label htmlFor="sortByGold">금메달 수 정렬</label>
-            <input defaultChecked={true} type="radio" name="SortOption" onChange={() => {
-              handleSortOption("goldMedal")
-            }} />
+            <input
+              defaultChecked={true}
+              type="radio"
+              name="SortOption"
+              onChange={() => {
+                handleSortOption("goldMedal");
+              }}
+            />
           </div>
-          <div className='option'>
+          <div className="option">
             <label htmlFor="sortByTotal">총메달 수 정렬</label>
-            <input type="radio" name="SortOption" onChange={() => {
-              handleSortOption("totalMedal")
-            }} />
+            <input
+              type="radio"
+              name="SortOption"
+              onChange={() => {
+                handleSortOption("totalMedal");
+              }}
+            />
           </div>
         </div>
         <section>
           <Form handleCountryList={handleCountryList} />
-        </section >
-
+        </section>
         {/* {테이블섹션} */}
         <section>
-          <Table countryList={countryList} handleCountryList={handleCountryList} />
+          <Table
+            countryList={countryList}
+            handleCountryList={handleCountryList}
+          />
         </section>
-      </main >
+      </main>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 
 const Form = ({ handleCountryList }) => {
   const [country, setCountry] = useState("");
   const [gold, setGold] = useState(0);
   const [silver, setSilver] = useState(0);
-  const [bronze, setBonze] = useState(0);
+  const [bronze, setBronze] = useState(0);
 
   //입력폼을 초기화시켜주는 함수
   const clearInput = () => {
-    setCountry("")
-    setGold(0)
-    setSilver(0)
-    setBonze(0)
-  }
+    setCountry("");
+    setGold(0);
+    setSilver(0);
+    setBronze(0);
+    return;
+  };
 
   //폼 제출시 실행
   const handleSubmit = (e) => {
@@ -23,49 +23,113 @@ const Form = ({ handleCountryList }) => {
       goldMedalCount: gold,
       silverMedalCount: silver,
       bronzeMedalCount: bronze,
-    }
-    handleCountryList.addCountry(newCountry) && clearInput()
-  }
+    };
+    handleCountryList.addCountry(newCountry) && clearInput();
+  };
 
-  //업데이트버튼클릭시 실행 
+  //업데이트버튼클릭시 실행
   const handleUpdate = () => {
     const updateCountry = {
       countryName: country,
       goldMedalCount: gold,
       silverMedalCount: silver,
       bronzeMedalCount: bronze,
-    }
-    handleCountryList.updateCountry(updateCountry) && clearInput()
-  }
+    };
+    handleCountryList.updateCountry(updateCountry) && clearInput();
+  };
 
+  //(TIL)
+  //NaN이 넘버벨류라서 오류...
+  //NaN인지 판별해주는함수(Number.isNaN(gold)
+
+  // useEffect(() => {
+  //   if (Number.isNaN(silver)) setSilver(0);
+  // }, [silver]);
+
+  // useEffect(() => {
+  //   if (Number.isNaN(bronze)) setBronze(0);
+  // }, [bronze]);
+  // //useEffect()는 외부 데이터를 동기화 할때만 사용할 것
+
+  //(TIL)
+  //input type number 사용하지말기(00001같은 숫자가 이상하게 처리됨)
+  //검증하는건 js코드로 직접하기
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <div className='input-group'>
+        <div className="input-group">
           <label htmlFor="country">국가명</label>
-          <input value={country} onChange={(e) => { setCountry(e.target.value) }} type="text" id="country" placeholder="한글 국가명을 입력하세요" required />
+          <input
+            value={country}
+            onChange={(e) => {
+              setCountry(e.target.value);
+            }}
+            type="text"
+            id="country"
+            placeholder="국가명을 입력하세요"
+          />
         </div>
 
-        <div className='input-group'>
+        <div className="input-group">
           <label htmlFor="gold">금메달</label>
-          <input value={gold} onChange={(e) => { setGold(e.target.value) }} type="number" id="gold" min="0" placeholder="금메달 수" required />
+          <input
+            value={gold}
+            onChange={(e) => {
+              if (Number.isNaN(+e.target.value) || +e.target.value < 0) {
+                alert("숫자만입력");
+                return;
+              }
+              setGold(+e.target.value);
+            }}
+            type="text"
+            id="gold"
+            placeholder="금메달 수"
+          />
         </div>
 
-        <div className='input-group'>
+        <div className="input-group">
           <label htmlFor="silver">은메달</label>
-          <input value={silver} onChange={(e) => { setSilver(e.target.value) }} type="number" id="silver" min="0" placeholder="은메달 수" required />
+          <input
+            value={silver}
+            onChange={(e) => {
+              if (Number.isNaN(+e.target.value) || +e.target.value < 0) {
+                alert("숫자만입력");
+                return;
+              }
+              setSilver(+e.target.value);
+            }}
+            type="text"
+            id="silver"
+            placeholder="은메달 수"
+          />
         </div>
 
-        <div className='input-group'>
+        <div className="input-group">
           <label htmlFor="bronze">동메달</label>
-          <input value={bronze} onChange={(e) => { setBonze(e.target.value) }} type="number" id="bronze" min="0" placeholder="동메달 수" required />
+          <input
+            value={bronze}
+            onChange={(e) => {
+              if (Number.isNaN(+e.target.value) || +e.target.value < 0) {
+                alert("숫자만입력");
+                return;
+              }
+              setBronze(+e.target.value);
+            }}
+            type="text"
+            id="bronze"
+            placeholder="동메달 수"
+          />
         </div>
-        <button type="submit" name="add">국가추가</button>
-        <button type="button" onClick={handleUpdate} name="update">업데이트</button>
-      </form >
-    </>
-  )
-}
 
+        <button type="submit" name="add">
+          국가추가
+        </button>
+        <button type="button" onClick={handleUpdate} name="update">
+          업데이트
+        </button>
+      </form>
+    </>
+  );
+};
 
 export default Form;
